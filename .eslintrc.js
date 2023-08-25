574c64ff03176086edf30367b9eb8b7d49d69ff4
viglino <jean-marc.viglino@ign.fr>
2023-08-25 14:34:19 +0200

    [BUILD]

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  "env": {
+    "browser": true,
+    "es2021": true
+  },
+  "ignorePatterns": [".*", "**/www/*", "**/todo/*", "**/public/*"],
+  "extends": "eslint:recommended",
+  "parserOptions": {
+    "ecmaVersion": 12,
+    "sourceType": "module"
+  },
+  "rules": {
+  }
+};
